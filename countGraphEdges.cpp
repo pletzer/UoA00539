f1af8c6b79a0cd35c8e46f73434f03fc8925ddf5
Alex Pletzer <alexander@gokliya.net>
2019-02-08 03:03:56 +0000

    code simplification

--- countGraphEdges.cpp
+++ countGraphEdges.cpp
@@ -23,21 +23,20 @@ void mexFunction(int nlhs, mxArray *plhs[],
     for (size_t b = a + 2; b <= ln; ++b) {
       size_t bm1 = b - 1;
       float yb = y[bm1];
-      float bma = (float)(b) - (float)(a);
+      float bma = (float)(bm1) - (float)(am1);
       float coeff = (ya - yb) / bma;
       double fl = 1;
-      size_t c = a + 1;
-      while (c <= b - 1) {
-        size_t cm1 = c - 1;
-        float bmc = (float)(b) - (float)(c);
+      size_t cm1 = am1 + 1;
+      while (cm1 <= bm1 - 1) {
+        float bmc = (float)(bm1) - (float)(cm1);
         if (y[cm1] >= yb + coeff * bmc) {
             fl = 0;
             break;
         }
-        c++;
+        cm1++;
       }
       //add one to the graph edges of length b-a
-      P[bm1-a] += fl;
+      P[bm1-am1-1] += fl;
     }
   }
 
